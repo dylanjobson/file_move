f4c2f44b9ca26e459f1bda07e1b20fa32bbb9fb5
dylanjobson <36141483+dylanjobson@users.noreply.github.com>
2018-11-25 19:59:14 -0600

    Update file_move.py

--- file_move.py
+++ file_move.py
@@ -2,11 +2,16 @@ import os
 import shutil
 import glob
 
-source = 'YOUR PATH HERE'  #source of files you want to move
-destination = 'YOUR PATH HERE'  #destination for files 
-extension = '*.jpg'  #extension of files you want to move
-files = os.listdir(source)
+#source of files you want to move
+source = 'YOUR PATH HERE'  
 
+#destination for files 
+destination = 'YOUR PATH HERE'  
+
+ #extension of files you want to move
+extension = '*.jpg' 
+
+file = os.listdir(source)
 
 for file in glob.glob(source+(extension)):
     shutil.move(file, destination) 
